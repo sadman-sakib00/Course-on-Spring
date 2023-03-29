a5cb8540e64abf4731e2291d7fb567df00d52975
ChasedByDeath <sakibs100@gmail.com>
2023-03-30 04:31:30 +0600

    Literal values injection

--- IntroductionToSpring/src/com/sakib/learningspring/CricketCoach.java
+++ IntroductionToSpring/src/com/sakib/learningspring/CricketCoach.java
@@ -2,13 +2,38 @@ package com.sakib.learningspring;
 
 public class CricketCoach implements Coach {
 
+    private String emailAddress;
+    private String team;
     private FortuneService fortuneService;
 
+    // A constructor with no arguments
     public CricketCoach() {
+        System.out.println("I am in the constructor of CricketCoach");
+    }
 
+    // Setter method
+
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method  - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method  - setTeam");
+        this.team = team;
     }
 
     public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("I am in the setFortuneService method of CricketCoach class");
         this.fortuneService = fortuneService;
     }
 
