b32b63b98f769d15e29849af168581412c7f2761
sakib-bs <sadman.sakib@brainstation-23.com>
2023-04-04 15:52:48 +0600

    Adds inital state of IntroductionToMVC

---
+++ IntroductionToSpringMVC/src/main/java/com/sakib/learningspring/HomeController.java
@@ -0,0 +1,14 @@
+package com.sakib.learningspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu";
+	}
+	
+}
