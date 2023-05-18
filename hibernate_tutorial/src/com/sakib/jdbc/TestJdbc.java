0688bea05fb368b688e5609c06088e0942a2af63
sakib-bs <sadman.sakib@brainstation-23.com>
2023-05-18 20:30:23 +0600

    Adds testjdbc code

--- hibernate_tutorial/src/com/sakib/jdbc/TestJdbc.java
+++ hibernate_tutorial/src/com/sakib/jdbc/TestJdbc.java
@@ -1,10 +1,26 @@
 package com.sakib.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class TestJdbc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=UTC";
+		String user = "sakib";
+		String pass = "password";
+		
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			Connection connection = 
+					DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Connection successful!!!");
+			
+		} catch(Exception exception) {
+			exception.printStackTrace();
+		}
 	}
 
 }
